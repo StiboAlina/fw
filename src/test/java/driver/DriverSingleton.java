80d3f50a3d3dbf1033f3fbcefe05c2b8d06e0dac
StiboAlina <54958611+StiboAlina@users.noreply.github.com>
2021-01-26 12:01:00 +0300

    Update DriverSingleton.java

--- src/test/java/driver/DriverSingleton.java
+++ src/test/java/driver/DriverSingleton.java
@@ -11,20 +11,12 @@ public class DriverSingleton {
 
     private DriverSingleton(){}
     public static WebDriver getDriver(){
-        if(null==driver){
-            switch (System.getProperty("browser")){
-                case "firefox":{
-                    WebDriverManager.firefoxdriver().setup();
-                    driver=new FirefoxDriver();
-                }
-                default: {
+       
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
-                }
-
-            }
+                
             driver.manage().window().maximize();
-        }
+
         return driver;
     }
     public static void closeDriver(){
